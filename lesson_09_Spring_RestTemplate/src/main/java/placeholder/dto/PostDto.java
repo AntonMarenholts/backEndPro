9061b2dea10f5897d5251419d70ac5dd48f57a0d
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-06-18 17:50:45 +0200

    add lesson 09

--- lesson_09_Spring_RestTemplate/src/main/java/placeholder/dto/PostDto.java
+++ lesson_09_Spring_RestTemplate/src/main/java/placeholder/dto/PostDto.java
@@ -1,4 +1,18 @@
 package placeholder.dto;
 
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode(of = {"id"})
+
 public class PostDto {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
 }
